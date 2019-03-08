72fb67584f7617a35c332cbcdc11df952d4edde3
Saranyaroja <47551468+Saranyaroja@users.noreply.github.com>
2019-03-08 10:09:20 +0530

    Create zero.java

---
+++ zero.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*; 
+class number
+{
+    public static void main(String []s)
+    {
+        int num;
+        Scanner sc=new Scanner(System.in);  
+        num=sc.nextInt();         
+        if(num>0)
+        {
+            System.out.println("Positive");
+        }
+        else if(num<0)
+        {
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }        
+    }
+}
